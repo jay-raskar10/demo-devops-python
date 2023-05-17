a3ca0ef111148a654651230818e00038f318d8a7
Jay Raskar <jayraskar2003@gmail.com>
2023-05-17 22:28:06 +0530

    message changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello World, This is Jay.</h1>"
+    return "<h1>Hello World, This Project.</h1>"
 
 if __name__ == "__main__":
     app.run()
