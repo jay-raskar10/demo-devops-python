4d44a6d7844a28c82472bb3dcf38f9deaae670f9
Jay Raskar <jayraskar2003@gmail.com>
2023-05-19 15:13:12 +0530

    demo commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>This html file is rendered in flask application which is hosted on server.</h1>"
+    return "<h1>This html file.</h1>"
 
 if __name__ == "__main__":
     app.run()
