38e9d6fe140e102cd4617dc345630f35da15b4eb
Jay Raskar <jayraskar2003@gmail.com>
2023-05-20 09:45:49 +0530

    s-updation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>This html file,is rendered in flask and hosted on server.</h1>"
+    return "<h1>This html file,is rendered in flask and hosted on server,& is accessible globally</h1>"
 
 if __name__ == "__main__":
     app.run()
